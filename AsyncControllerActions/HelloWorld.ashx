f5db3875752799a195e494edd67543b9c312e872
Jason J. Rotello <jason@jrotello.com>
2014-12-01 22:58:25 -0600

    Added generic handler (.ashx)

---
+++ AsyncControllerActions/HelloWorld.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="HelloWorld.ashx.cs" Class="AsyncControllerActions.HelloWorld" %>
---
+++ AsyncControllerActions/HelloWorld.ashx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AsyncControllerActions
+{
+    /// <summary>
+    /// Summary description for HelloWorld
+    /// </summary>
+    public class HelloWorld : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
