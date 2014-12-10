1d58cdcc6082a8ea414f0b46c81c97494c95f5dc
Jason J. Rotello <jason@jrotello.com>
2014-12-10 09:01:52 -0600

    Added a simple 'delay request' module.

---
+++ AsyncControllerActions/DelayRequestModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AsyncControllerActions
+{
+    public class DelayRequestModule: IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += OnBeginRequestHandler;
+        }
+
+        private void OnBeginRequestHandler(object sender, EventArgs eventArgs)
+        {
+            var application = (HttpApplication) sender;
+            string value = application.Request.QueryString["delayRequest"];
+            if (!string.IsNullOrEmpty(value))
+            {
+                int delay;
+                if (int.TryParse(value, out delay))
+                {
+                    Task.Delay(delay).Wait();
+                }
+            }
+        }
+        public void Dispose() { }
+    }
+}
